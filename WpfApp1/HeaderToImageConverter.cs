1995c216cb71530b2794b15e646a76f71e02adf0
“dellwin10” <dellwin10@example.com>
2019-12-30 14:49:02 +0800

    wpfTreeview提交

---
+++ WpfApp1/HeaderToImageConverter.cs
@@ -0,0 +1,73 @@
+﻿/***********************************************************************
+*  Copyright  (c) 2019$regisrereddorganization$ All Rights Reserved
+*  CLR版本：  4.0.30319.42000
+*  机器名称:  DELL
+*  公司名称： WpfApp1
+*  创建时间:  2019/12/30 13:45:51
+*  文件名:    HeaderToImageConverter
+*  版本号：    V1.0.0.0
+*  唯一标识：  984f69d2-2689-4608-b505-71069e8410a4
+*  当前的用户域： DELL
+*  创建人：    admin
+*  电子邮箱：  yorkdisfu@gmail.com
+
+* 描述:
+* 
+* =======================================================================
+* 
+* 修改标记
+* 修改时间:2019/12/30 13:45:51
+* 修改人:admin
+* 版本号：V1.0.0.0
+*
+* 描述
+*
+*
+/***********************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// converts a full path to a specific image type of 
+    /// </summary>
+    /// []
+    [ValueConversion(typeof(string),typeof(BitmapImage))]
+    public class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //return new object();
+            var path = (string)value;
+            if (path ==null)
+            {
+                return null;
+            }
+            var name = WpfTreeView.GetFileFolderName(path);
+            var image = "Images/driver.jpg";
+            if (string.IsNullOrEmpty(name))
+            {
+                image = "Images/f1.jpg";
+            }
+            else if(new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+            {
+                image = "Images/f2.jpg";
+            }
+           
+            return new BitmapImage(new Uri($"pack://application:,,,/{image}"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WpfApp1/WpfTreeView.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+using System.IO;
+
+namespace WpfApp1
+{
+
+    
+    /// <summary>
+    /// WpfTreeView.xaml 的交互逻辑
+    /// </summary>
+    public partial class WpfTreeView : Window
+    {
+        #region constructor
+        public WpfTreeView()
+        {
+            InitializeComponent();
+        }
+
+
+        #endregion
+        #region on load
+        /// <summary>
+        /// when the application first opens
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //get every logical drive on the machine
+            foreach (var drive in Directory.GetLogicalDrives())
+            {
+                //create a new item fot it 
+                var item = new TreeViewItem() {
+                    //set the header and path 
+                   Header = drive,
+                //and a full path 
+                Tag = drive
+            };
+                
+
+
+                //add a domain item
+                item.Items.Add(null);
+
+                //listen out for item being expanded
+                item.Expanded += Folder_Expanded;
+                //add it to the main  treeview 
+                FolderView.Items.Add(item);
+            }
+
+        }
+        #endregion
+        private void Folder_Expanded(object sender, RoutedEventArgs e )
+        {
+            var item = (TreeViewItem)sender;
+            if (item.Items.Count != 1 || item.Items[0] != null)
+            {
+                return;
+
+            }
+            item.Items.Clear();
+            var fullPath = (string)item.Tag;
+            //create a blank list for directories 
+            var diretories = new List<string>();
+            //try and get directories from the floder ,ignore any issues doing so 
+            try
+            {
+                var dirs = Directory.GetDirectories(fullPath);
+                if(dirs.Length>0)
+                {
+                    diretories.AddRange(dirs);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            diretories.ForEach(directoryPath => {
+                var subItem = new TreeViewItem()
+                {
+                    //set header as Folder name
+                    //Header = Path.GetDirectoryName(directoryPath),
+                    Header = GetFileFolderName(directoryPath),
+                    //and tag a full path 
+                    Tag = directoryPath
+                };
+                //AddChild dummy item so we can expand folder 
+                subItem.Items.Add(null);
+                subItem.Expanded += Folder_Expanded;
+
+                item.Items.Add(subItem);
+            });
+        }
+        public static string GetFileFolderName(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                return string.Empty;
+            }
+
+            var normalizedPath = path.Replace('/','\\');
+            var lastIndex = normalizedPath.LastIndexOf('\\');
+            if (lastIndex <=0)
+            {
+                return path;
+            }
+            return path.Substring(lastIndex+1);
+
+        }
+    }
+}
